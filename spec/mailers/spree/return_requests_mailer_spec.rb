3241913b43367c0f5412087b677241e384375078
Brady Somerville <brady.somerville@gmail.com>
2014-01-18 23:31:22 -0600

    Fix mailer spec

--- spec/mailers/spree/return_requests_mailer_spec.rb
+++ spec/mailers/spree/return_requests_mailer_spec.rb
@@ -1,31 +1,35 @@
 require "spec_helper"
 
 describe Spree::ReturnRequestsMailer do
+
+  let(:order) { FactoryGirl.build(:shipped_order) }
+  let(:return_request) { FactoryGirl.build(:spree_return_request, order: order, email_address: "test@example.com") }
+
   describe "submitted" do
-    let(:mail) { Spree::ReturnRequestsMailer.submitted }
+    let(:mail) { Spree::ReturnRequestsMailer.submitted(return_request) }
 
     it "renders the headers" do
       mail.subject.should eq("Thank you for submitting your return request")
-      mail.to.should eq(["to@example.org"])
+      mail.to.should eq([return_request.email_address])
       mail.from.should eq(["from@example.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("Thank you for submitting")
     end
   end
 
   describe "approved" do
-    let(:mail) { Spree::ReturnRequestsMailer.approved }
+    let(:mail) { Spree::ReturnRequestsMailer.approved(return_request) }
 
     it "renders the headers" do
-      mail.subject.should eq("Approved")
-      mail.to.should eq(["to@example.org"])
+      mail.subject.should eq("Your return request has been approved")
+      mail.to.should eq([return_request.email_address])
       mail.from.should eq(["from@example.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("Your return request has been approved")
     end
   end
 
