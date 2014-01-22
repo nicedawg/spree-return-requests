2ff70e5cb679a52fac77c021aef99166a417ad18
Brady Somerville <brady.somerville@gmail.com>
2014-01-22 00:18:47 -0600

    Use spree config for to/from addresses for mailer

--- app/mailers/spree/return_requests_mailer.rb
+++ app/mailers/spree/return_requests_mailer.rb
@@ -1,32 +1,22 @@
 class Spree::ReturnRequestsMailer < ActionMailer::Base
-  default from: "from@example.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.spree.return_requests.submitted.subject
-  #
   def submitted(return_request)
-    mail to: return_request.email_address, subject: "Thank you for submitting your return request"
+    mail to: return_request.email_address, from: site_owner_email, subject: "Thank you for submitting your return request"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.spree.return_requests.submitted_admin.subject
-  #
   def submitted_admin(return_request)
     @return_request = return_request
-    mail to: SpreeReturnRequests::Config[:return_request_admin_notification_email], subject: "A customer submitted a return request"
+    mail to: site_owner_email, from: site_owner_email, subject: "A customer submitted a return request"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.spree.return_requests.approved.subject
-  #
   def approved(return_request)
     @return_request = return_request
-    mail to: return_request.email_address, subject: "Your return request has been approved"
+    mail to: return_request.email_address, from: site_owner_email, subject: "Your return request has been approved"
   end
+
+  private
+
+    def site_owner_email
+      Spree::Config[:mails_from]
+    end
 end
