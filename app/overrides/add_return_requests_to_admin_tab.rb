adabec2a8253337c03ea332fbe298680a0a976da
Brady Somerville <brady.somerville@gmail.com>
2014-07-15 11:46:43 -0500

    Add data hook to admin tab for easy hookin'

--- app/overrides/add_return_requests_to_admin_tab.rb
+++ app/overrides/add_return_requests_to_admin_tab.rb
@@ -3,7 +3,7 @@ Deface::Override.new(
   name: 'add_return_requests_to_admin_tab',
   insert_top: '[data-hook="admin_tabs"]',
   text: '
-  <li class="tab-with-icon <%= params[:controller] == "spree/admin/return_requests" ? "selected" : "" %>">
+  <li class="tab-with-icon <%= params[:controller] == "spree/admin/return_requests" ? "selected" : "" %>" data-hook="return-requests-admin-tab">
     <%= link_to admin_return_requests_path, class: "icon_link with-tip icon-ambulance return_requests" do %>
       <span class="text">Return Requests</span>
     <% end %>
